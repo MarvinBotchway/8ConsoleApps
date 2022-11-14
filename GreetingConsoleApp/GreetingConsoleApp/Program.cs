b29e76b784763af48503bd6b924b0ee731d09545
Marvin Botchway <marvinbotchway@outlook.com>
2022-11-14 19:54:17 +0000

    Create GreatingConsoleApp

--- GreetingConsoleApp/GreetingConsoleApp/Program.cs
+++ GreetingConsoleApp/GreetingConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// We first ask for the users name using the method below
+// This particular method writes the prompt in the parenthesis
+// and lets the user write their input on the next line hence "WriteLine"
+// and press enter
+Console.WriteLine("Please Enter Your First Name and Press Enter: ");
+
+// We then create a string variable, take the input by reading the line using the method below
+// Then assign the value to the variable
+string usersFirstName = Console.ReadLine();
+
+Console.WriteLine($"Hey there! {usersFirstName} ");
